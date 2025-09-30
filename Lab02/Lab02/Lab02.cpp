208dc3a3c1d922fb60b0fc17657e21439d8ca5ce
Hayden Olson <haydenolson@byui.edu>
2025-09-30 14:09:04 -0600

    FIxed some text

--- Lab02/Lab02/Lab02.cpp
+++ Lab02/Lab02/Lab02.cpp
@@ -2,7 +2,7 @@
  * 1. Name:
  *      -Hayden Olson & Mason Allen-
  * 2. Assignment Name:
- *      Practice 02: Physics simulator
+ *      Lab 02: Apollo 11 Physics
  * 3. Assignment Description:
  *      Compute how the Apollo lander will move across the screen
  * 4. What was the hardest part? Be as specific as possible.
