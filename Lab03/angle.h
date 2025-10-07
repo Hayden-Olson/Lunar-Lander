f2781763e7e07bc58cc8db678a4e72b6410d8138
Hayden Olson <haydenolson@byui.edu>
2025-10-07 17:25:51 -0600

    the angle.h file should not be altered. the angle.cpp file is where we apply the inside logic of the functions for the .h file.

--- Lab03/angle.h
+++ Lab03/angle.h
@@ -18,79 +18,39 @@ class TestAcceleration;
 class TestAngle;
 class TestLander;
 
- /************************************
-  * ANGLE
-  ************************************/
+/************************************
+ * ANGLE
+ ************************************/
 class Angle
 {
 public:
-   friend TestAcceleration;
-   friend TestVelocity;
-   friend TestAngle;
-   friend TestLander;
-   
-   // Constructors
-   Angle() : radians(-99.9)
-   {
+	friend TestAcceleration;
+	friend TestVelocity;
+	friend TestAngle;
+	friend TestLander;
 
-   };
-   Angle(const Angle& rhs) : radians(-99.9)
-   {
+	// Constructors
+	Angle() : radians(-99.9) {}
+	Angle(const Angle& rhs) : radians(-99.9) {}
+	Angle(double degrees) : radians(-99.9) {}
 
-   };
-   Angle(double degrees) : radians(-99.9)
-   {
+	// Getters
+	double getDegrees() const { return -99.9; }
+	double getRadians() const { return -99.9; }
 
-   };
-
-   // Getters
-   double getDegrees() const { return -99.9; };
-   double getRadians() const { return -99.9; };
-
-   // Setters
-   void setDegrees(double degrees)
-   {
-	   radians  = degrees / 360 * (M_PI * 2);
-   };
-   void setRadians(double inRadians)
-   {
-	   radians = inRadians;
-   };
-   void setUp()
-   {
-	   setDegrees(0);
-   };
-   void setDown()
-   {
-	   setDegrees(180);
-   };
-   void setRight()
-   {
-	   setDegrees(90);
-   };
-   void setLeft()
-   {
-	   setDegrees(270);
-   };
-   void reverse()
-   {
-	   //not sure what this is supposed to do
-   };
-   Angle& add(double delta) { radians = -99.9; return *this; }
+	// Setters
+	void setDegrees(double degrees) {}
+	void setRadians(double radians) {}
+	void setUp() {}
+	void setDown() {}
+	void setRight() {}
+	void setLeft() {}
+	void reverse() {}
+	Angle& add(double delta) { radians = -99.9; return *this; }
 
 private:
-   double normalize(double radians) const;
+	double normalize(double radians) const;
 
-   double radians;   // 360 degrees equals 2 PI radians
-
-   double convertToDegrees(double radians)
-	{
-	   return radians / (M_PI * 2) * 360;
-    };
-
-	double convertToRadians(double degrees)
-   {
-	   return degrees / 360 * (M_PI * 2);
-   };
+	double radians;   // 360 degrees equals 2 PI radians
 };
 
