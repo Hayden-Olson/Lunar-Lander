ec2c508b14294e79ba5f3d0af5327428b06b26f0
Hayden Olson <haydenolson@byui.edu>
2025-10-18 15:53:50 -0600

    i think the class is working? theres no longer any errors at least.

--- Lab05/star.h
+++ Lab05/star.h
@@ -18,21 +18,20 @@
 class Star
 {
 private:
-	Position pos;
-	unsigned char phase;
+   Position pos;
+   unsigned char phase;
 public:
-	void reset(double width, double height)
-	{
-		double x = random(0.0, width);
-		double y = random(0.0, height);
-		pos.setX(x);
-		pos.setY(y);
-		phase = random(0, 3);
-	}
-	void draw(ogstream gout) const
-	{
-		for (int i = 0; i < 51; i++)
-			gout.drawStar(pos, phase);
-			phase = (phase + 1) % 4;
-	}
+   void reset(double width, double height)
+   {
+      double x = random(0.0, width);
+      double y = random(0.0, height);
+      pos.setX(x);
+      pos.setY(y);
+   }
+   void draw(ogstream gout)
+   {
+      for (int i = 0; i < 51; i++)
+         gout.drawStar(pos, phase);
+         phase += 1;
+   }
 };
