c78319c52eb255d6ab9cc07bc504894b58b4a236
Hayden Olson <haydenolson@byui.edu>
2025-10-28 13:24:37 -0600

    all variables and functions are added in as stubs.

--- Pointer/wallet.h
+++ Pointer/wallet.h
@@ -1,6 +1,6 @@
 /*************************************************************
  * 1. Name:
- *      -your name-
+ *      Hayden Olson & Mason Allen
  * 2. Module
  *      WALLET
  * 3. Assignment Description:
@@ -20,5 +20,62 @@ class Wallet
 {
    friend TestWallet;
 
+private:
+	double accountSam;
+	double accountSue;
+	double* pCurrentAccount;
+
+public:
+	Wallet()
+	{
+		accountSam = 0.00;
+		accountSue = 0.00;
+		pCurrentAccount = &accountSam;
+	}
+
+	void updateSam(double amount)
+	{
+		accountSam += amount;
+	}
+
+	void updateSue(double amount)
+	{
+		accountSue += amount;
+	}
+
+	void updateCurrent(double amount)
+	{
+		*pCurrentAccount += amount;
+	}
+
+	int chooseSam()
+	{
+		return 0;
+	}
+
+	int chooseSue()
+	{
+		return 1;
+	}
+
+	int chooseCurrent()
+	{
+		return 0;
+	}
+
+	double getSam()
+	{
+		return 0.00;
+	}
+
+	double getSue()
+	{
+		return 0.00;
+	}
+
+	double getCurrent()
+	{
+		return 0.00;
+	}
 };
 
